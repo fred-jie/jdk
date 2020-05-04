9fe4dc23d866db501f6e50033d8ebf01ac0978a6
永瑾 <15857346961@163.com>
2020-05-04 10:05:28 +0800

    jdk

---
+++ src/com/FredJie/test/java/util/hashMap/HashMapTest.java
@@ -0,0 +1,23 @@
+package com.FredJie.test.java.util.hashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ProjectName: jdk
+ * @Package: com.FredJie.test.java.util.hashMap
+ * @ClassName: hashMap
+ * @Description:
+ * @Author: Fredjie
+ * @CreateDate: 2020/5/4 9:57
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2020/5/4 9:57
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("x", "sd");
+    }
+}
